0f3bee5f5d2b0343acae1b4ea3ef652c5b853646
Silthus <silthus@raid-craft.de>
2012-11-14 12:52:11 +0100

    example implementation of the Spells Component combined with an active Skill

---
+++ src/main/java/de/raidcraft/skills/skills/magic/Fireball.java
@@ -0,0 +1,32 @@
+package de.raidcraft.skills.skills.magic;
+
+import de.raidcraft.api.player.RCPlayer;
+import de.raidcraft.skills.api.Active;
+import de.raidcraft.skills.api.skill.AbstractLevelableSkill;
+import de.raidcraft.skills.trigger.InteractTrigger;
+import de.raidcraft.spells.fire.RCFireball;
+import org.bukkit.event.block.Action;
+
+/**
+ * @author Silthus
+ */
+public class Fireball extends AbstractLevelableSkill implements Active<InteractTrigger> {
+
+    public Fireball(int id, RCPlayer player) {
+
+        super(id, player);
+    }
+
+    @Override
+    public void run(InteractTrigger trigger) {
+
+        if (trigger.getAction() == Action.RIGHT_CLICK_AIR) {
+
+            RCFireball fireball = new RCFireball();
+            // TODO: set variable strength of the fireball based on the skill level
+            fireball.fireTicks = 60;
+            fireball.incinerate = true;
+            fireball.run(getPlayer().getBukkitPlayer());
+        }
+    }
+}
