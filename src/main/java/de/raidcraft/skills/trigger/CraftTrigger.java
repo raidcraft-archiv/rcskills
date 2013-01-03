75a3d80bc1c12cf228783797fee6e459149b87d8
Silthus <silthus@raid-craft.de>
2013-01-03 19:25:15 +0100

    added enchant and craft trigger

---
+++ src/main/java/de/raidcraft/skills/trigger/CraftTrigger.java
@@ -0,0 +1,31 @@
+package de.raidcraft.skills.trigger;
+
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.trigger.BukkitEventTrigger;
+import de.raidcraft.skills.api.trigger.HandlerList;
+import org.bukkit.event.inventory.CraftItemEvent;
+
+/**
+ * @author Silthus
+ */
+public class CraftTrigger extends BukkitEventTrigger<CraftItemEvent> {
+
+    public CraftTrigger(Hero hero, CraftItemEvent event) {
+
+        super(hero, event);
+    }
+
+    /*///////////////////////////////////////////////////
+    //              Needed Trigger Stuff
+    ///////////////////////////////////////////////////*/
+
+    private static final HandlerList handlers = new HandlerList();
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
