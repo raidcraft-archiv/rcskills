d772c72eacfff57fa22b11875273655e3dce3a83
Silthus <silthus@raid-craft.de>
2013-01-06 11:49:10 +0100

    added buff and debuff interface to display the buffs a player has

---
+++ src/main/java/de/raidcraft/skills/api/effect/Buff.java
@@ -0,0 +1,9 @@
+package de.raidcraft.skills.api.effect;
+
+/**
+ * @author Silthus
+ */
+public interface Buff {
+
+    public String displayBuff();
+}
---
+++ src/main/java/de/raidcraft/skills/api/effect/Debuff.java
@@ -0,0 +1,9 @@
+package de.raidcraft.skills.api.effect;
+
+/**
+ * @author Silthus
+ */
+public interface Debuff {
+
+    public void displayDebuff();
+}
