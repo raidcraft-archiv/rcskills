5d03835f8730a8f9dc4dc9fdff907ea439e24aba
Silthus <silthus@raid-craft.de>
2013-01-06 15:50:17 +0100

    added possibility to get a custom config: CustonConfig.getConfig(name)

---
+++ src/main/java/de/raidcraft/skills/config/CustomConfig.java
@@ -0,0 +1,37 @@
+package de.raidcraft.skills.config;
+
+import de.raidcraft.RaidCraft;
+import de.raidcraft.api.config.ConfigurationBase;
+import de.raidcraft.skills.SkillsPlugin;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Silthus
+ */
+public class CustomConfig extends ConfigurationBase<SkillsPlugin> {
+
+    private static final SkillsPlugin PLUGIN = RaidCraft.getComponent(SkillsPlugin.class);
+    private static final Map<String, CustomConfig> customConfigs = new HashMap<>();
+
+    public CustomConfig(SkillsPlugin plugin, File file) {
+
+        super(plugin, file);
+    }
+
+    public static CustomConfig getConfig(String name) {
+
+        CustomConfig config;
+        if (!customConfigs.containsKey(name)) {
+            File dir = new File(PLUGIN.getDataFolder(), "custom-configs");
+            dir.mkdirs();
+            config = new CustomConfig(PLUGIN, new File(dir, name));
+            customConfigs.put(name, config);
+        } else {
+            config = customConfigs.get(name);
+        }
+        return config;
+    }
+}
