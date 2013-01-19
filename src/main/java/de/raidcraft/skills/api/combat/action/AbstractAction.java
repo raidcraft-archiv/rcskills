737d82983d0d65bb23517d157a1b193835bb9d94
Silthus <silthus@raid-craft.de>
2013-01-19 16:10:33 +0100

    a combat action now always triggers combat

--- src/main/java/de/raidcraft/skills/api/combat/action/AbstractAction.java
+++ src/main/java/de/raidcraft/skills/api/combat/action/AbstractAction.java
@@ -1,5 +1,10 @@
 package de.raidcraft.skills.api.combat.action;
 
+import de.raidcraft.RaidCraft;
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.effect.common.Combat;
+import de.raidcraft.skills.api.exceptions.CombatException;
+
 /**
  * @author Silthus
  */
@@ -10,6 +15,14 @@ public abstract class AbstractAction<T> implements Action<T> {
     protected AbstractAction(T source) {
 
         this.source = source;
+        // lets trigger the combat effect
+        if (source instanceof CharacterTemplate) {
+            try {
+                ((CharacterTemplate) source).addEffect(source, Combat.class);
+            } catch (CombatException e) {
+                RaidCraft.LOGGER.warning(e.getMessage());
+            }
+        }
     }
 
     @Override
