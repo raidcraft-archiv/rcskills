9dd51c89750a3791086dabb6565301dfe266d9b6
Silthus <silthus@raid-craft.de>
2013-02-06 19:44:51 +0100

    fixed stamina display

--- src/main/java/de/raidcraft/skills/api/resource/visual/Stamina.java
+++ src/main/java/de/raidcraft/skills/api/resource/visual/Stamina.java
@@ -14,7 +14,7 @@ public class Stamina implements VisualResource {
 
         Player player = resource.getHero().getPlayer();
         // set the stamina bar to a percentage of the actual stamina
-        int stamina = (int) Math.ceil(((double) resource.getCurrent() / resource.getMax()) * 20);
+        int stamina = (int) (((double) resource.getCurrent() / (double) resource.getMax()) * 20);
         player.setFoodLevel(stamina);
         // see the minecraft wiki for the mechanics: http://www.minecraftwiki.net/wiki/Hunger#Mechanics
         player.setSaturation(20.0F);
