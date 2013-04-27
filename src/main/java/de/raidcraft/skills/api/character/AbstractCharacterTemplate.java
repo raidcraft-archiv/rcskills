5ad1cadf83444738ecdc8944c661f338e742c33b
Silthus <silthus@raid-craft.de>
2013-04-27 15:55:40 +0200

    lets remove the entity after it died to make sure it died...

--- src/main/java/de/raidcraft/skills/api/character/AbstractCharacterTemplate.java
+++ src/main/java/de/raidcraft/skills/api/character/AbstractCharacterTemplate.java
@@ -23,7 +23,6 @@ import de.raidcraft.util.BukkitUtil;
 import de.raidcraft.util.EffectUtil;
 import de.raidcraft.util.LocationUtil;
 import de.raidcraft.util.MathUtil;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.EntityEffect;
 import org.bukkit.Location;
@@ -344,13 +343,7 @@ public abstract class AbstractCharacterTemplate implements CharacterTemplate {
         );
         // play the death effect
         getEntity().playEffect(EntityEffect.DEATH);
-        Bukkit.getScheduler().runTaskLater(RaidCraft.getComponent(SkillsPlugin.class), new Runnable() {
-            @Override
-            public void run() {
-
-                getEntity().remove();
-            }
-        }, 20L);
+        getEntity().remove();
     }
 
     public <E extends Effect> void addEffect(Class<E> eClass, E effect) throws CombatException {
