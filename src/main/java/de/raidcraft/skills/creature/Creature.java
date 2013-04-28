2c8619443eedb5bcfc343be71df01d32743f089d
Silthus <silthus@raid-craft.de>
2013-04-28 10:17:08 +0200

    each creature is now spawned with an average level that is calculated by the heroes in a 100 block radius (hero level!)

--- src/main/java/de/raidcraft/skills/creature/Creature.java
+++ src/main/java/de/raidcraft/skills/creature/Creature.java
@@ -5,18 +5,46 @@ import de.raidcraft.skills.SkillsPlugin;
 import de.raidcraft.skills.api.character.AbstractCharacterTemplate;
 import de.raidcraft.skills.api.character.CharacterTemplate;
 import de.raidcraft.skills.api.combat.action.Attack;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.level.AttachedLevel;
+import de.raidcraft.skills.api.level.Levelable;
+import org.bukkit.GameMode;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.PigZombie;
 import org.bukkit.entity.Wolf;
 
+import java.util.List;
+
 /**
  * @author Silthus
  */
-public class Creature extends AbstractCharacterTemplate {
+public class Creature extends AbstractCharacterTemplate implements Levelable<Creature> {
+
+    private AttachedLevel<Creature> attachedLevel;
 
     public Creature(LivingEntity entity) {
 
         super(entity);
+        // lets calculate the average level based on the players around
+        int averageLevel = 1;
+        try {
+            int totalLevel = 0;
+            int totalHeroes = 0;
+            List<CharacterTemplate> targets = getNearbyTargets(100, false);
+            for (CharacterTemplate target : targets) {
+                if (target instanceof Hero && ((Hero) target).getPlayer().getGameMode() != GameMode.CREATIVE) {
+                    totalHeroes++;
+                    totalLevel += ((Hero) target).getAttachedLevel().getLevel();
+                }
+            }
+            if (totalHeroes != 0) {
+                averageLevel = totalLevel / totalHeroes;
+            }
+        } catch (CombatException ignored) {
+            // ignored
+        }
+        attachLevel(new CreatureAttachedLevel<>(this, averageLevel));
     }
 
     @Override
@@ -45,5 +73,55 @@ public class Creature extends AbstractCharacterTemplate {
     public int getDefaultHealth() {
 
         return RaidCraft.getComponent(SkillsPlugin.class).getDamageManager().getCreatureHealth(getEntity().getType());
+    }
+
+    @Override
+    public AttachedLevel<Creature> getAttachedLevel() {
+
+        return attachedLevel;
+    }
+
+    @Override
+    public void attachLevel(AttachedLevel<Creature> attachedLevel) {
+
+        this.attachedLevel = attachedLevel;
+    }
+
+    @Override
+    public int getMaxLevel() {
+
+        return getAttachedLevel().getMaxLevel();
+    }
+
+    @Override
+    public void onExpGain(int exp) {
+
+    }
+
+    @Override
+    public void onExpLoss(int exp) {
+
+    }
+
+    @Override
+    public void onLevelGain() {
+
+    }
+
+    @Override
+    public void onLevelLoss() {
+
+    }
+
+    @Override
+    public boolean isMastered() {
+
+        return true;
+    }
+
+    @Override
+    public void saveLevelProgress(AttachedLevel<Creature> attachedLevel) {
+
+
     }
 }
