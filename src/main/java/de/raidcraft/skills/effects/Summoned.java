da943f0111a5f35358589e7a117128cd63c4905b
Silthus <silthus@raid-craft.de>
2013-06-06 15:49:25 +0200

    added summon skill checks for friendlies fixes #185

--- src/main/java/de/raidcraft/skills/effects/Summoned.java
+++ src/main/java/de/raidcraft/skills/effects/Summoned.java
@@ -7,6 +7,12 @@ import de.raidcraft.skills.api.effect.types.ExpirableEffect;
 import de.raidcraft.skills.api.exceptions.CombatException;
 import de.raidcraft.skills.api.persistance.EffectData;
 import de.raidcraft.skills.api.skill.Skill;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.TriggerPriority;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.trigger.AttackTrigger;
+import de.raidcraft.skills.trigger.EntityTargetTrigger;
+import org.bukkit.entity.LivingEntity;
 
 /**
  * @author Silthus
@@ -16,11 +22,43 @@ import de.raidcraft.skills.api.skill.Skill;
         description = "Markiert beschwörte Kreaturen.",
         types = {EffectType.SUMMON, EffectType.MAGICAL}
 )
-public class Summoned extends ExpirableEffect<Skill> {
+public class Summoned extends ExpirableEffect<Skill> implements Triggered {
 
     public Summoned(Skill source, CharacterTemplate target, EffectData data) {
 
         super(source, target, data);
+    }
+
+    @TriggerHandler(ignoreCancelled = true, priority = TriggerPriority.LOW)
+    public void onTargetTrigger(EntityTargetTrigger trigger) {
+
+        LivingEntity target = trigger.getEvent().getTarget();
+        if (target.equals(getSource().getHolder().getEntity())) {
+            trigger.getEvent().setCancelled(true);
+            return;
+        }
+        for (CharacterTemplate member : trigger.getSource().getParty().getMembers()) {
+            if (target.equals(member.getEntity())) {
+                trigger.getEvent().setCancelled(true);
+                return;
+            }
+        }
+    }
+
+    @TriggerHandler(ignoreCancelled = true, priority = TriggerPriority.LOW)
+    public void onAttack(AttackTrigger trigger) {
+
+        CharacterTemplate target = trigger.getAttack().getTarget();
+        if (target.equals(getSource().getHolder())) {
+            trigger.getAttack().setCancelled(true);
+            return;
+        }
+        for (CharacterTemplate member : trigger.getSource().getParty().getMembers()) {
+            if (target.equals(member)) {
+                trigger.getAttack().setCancelled(true);
+                return;
+            }
+        }
     }
 
     @Override
