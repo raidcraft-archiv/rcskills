e12c363e38838c2db99f9a8dc7cfd952017d27bb
Silthus <silthus@raid-craft.de>
2013-06-11 18:15:01 +0200

    always set the scoreboard again

--- src/main/java/de/raidcraft/skills/Scoreboards.java
+++ src/main/java/de/raidcraft/skills/Scoreboards.java
@@ -26,10 +26,12 @@ public final class Scoreboards {
     public static Scoreboard getScoreboard(Hero hero) {
 
         String playerName = hero.getName().toLowerCase();
+        Scoreboard scoreboard;
         if (scoreboards.containsKey(playerName)) {
-            return scoreboards.get(playerName);
+            scoreboard = scoreboards.get(playerName);
+        } else {
+            scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
         }
-        Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
         if (!hero.isOnline()) {
             hero.updateEntity(Bukkit.getPlayer(playerName));
         }
