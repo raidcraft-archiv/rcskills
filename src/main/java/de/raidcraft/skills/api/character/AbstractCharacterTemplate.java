ee0b04dca7e3a3d55163b2ff59bd411c422b90af
Silthus <silthus@raid-craft.de>
2013-09-22 14:46:36 +0200

    pull up the recalculate health

--- src/main/java/de/raidcraft/skills/api/character/AbstractCharacterTemplate.java
+++ src/main/java/de/raidcraft/skills/api/character/AbstractCharacterTemplate.java
@@ -379,6 +379,18 @@ public abstract class AbstractCharacterTemplate implements CharacterTemplate {
     }
 
     @Override
+    public void recalculateHealth() {
+
+        int maxHealth = getMaxHealth();
+        int defaultHealth = getDefaultHealth();
+        if (defaultHealth > maxHealth) {
+            increaseMaxHealth(defaultHealth - maxHealth);
+        } else if (defaultHealth < maxHealth) {
+            decreaseMaxHealth(maxHealth - defaultHealth);
+        }
+    }
+
+    @Override
     public int getHealth() {
 
         if (getEntity() == null) {
--- src/main/java/de/raidcraft/skills/api/hero/AbstractHero.java
+++ src/main/java/de/raidcraft/skills/api/hero/AbstractHero.java
@@ -636,18 +636,6 @@ public abstract class AbstractHero extends AbstractSkilledCharacter<Hero> implem
     }
 
     @Override
-    public void recalculateHealth() {
-
-        int maxHealth = getMaxHealth();
-        int defaultHealth = getDefaultHealth();
-        if (defaultHealth > maxHealth) {
-            increaseMaxHealth(defaultHealth - maxHealth);
-        } else if (defaultHealth < maxHealth) {
-            decreaseMaxHealth(maxHealth - defaultHealth);
-        }
-    }
-
-    @Override
     public void setHealth(int health) {
 
         super.setHealth(health);
