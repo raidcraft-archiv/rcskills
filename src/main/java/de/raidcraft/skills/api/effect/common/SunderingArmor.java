c44573723e92c8f241178daa5a47804f2acf6bc8
Silthus <silthus@raid-craft.de>
2013-10-05 23:26:40 +0200

    made sundering armor an ability skill

--- src/main/java/de/raidcraft/skills/api/effect/common/SunderingArmor.java
+++ src/main/java/de/raidcraft/skills/api/effect/common/SunderingArmor.java
@@ -1,12 +1,12 @@
 package de.raidcraft.skills.api.effect.common;
 
+import de.raidcraft.skills.api.ability.Ability;
 import de.raidcraft.skills.api.character.CharacterTemplate;
 import de.raidcraft.skills.api.combat.EffectType;
 import de.raidcraft.skills.api.effect.EffectInformation;
 import de.raidcraft.skills.api.effect.types.ExpirableEffect;
 import de.raidcraft.skills.api.exceptions.CombatException;
 import de.raidcraft.skills.api.persistance.EffectData;
-import de.raidcraft.skills.api.skill.Skill;
 import de.raidcraft.skills.util.ConfigUtil;
 import org.bukkit.configuration.ConfigurationSection;
 
@@ -19,13 +19,13 @@ import org.bukkit.configuration.ConfigurationSection;
         types = {EffectType.HARMFUL, EffectType.DEBUFF, EffectType.PHYSICAL},
         priority = 1.0
 )
-public class SunderingArmor extends ExpirableEffect<Skill> {
+public class SunderingArmor extends ExpirableEffect<Ability> {
 
     private double armorReduction = 0.05;
     private double armorReductionPerStack;
     private double armorReductionCap = 0.6;
 
-    public SunderingArmor(Skill source, CharacterTemplate target, EffectData data) {
+    public SunderingArmor(Ability source, CharacterTemplate target, EffectData data) {
 
         super(source, target, data);
     }
