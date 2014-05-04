ef772668e6a1a429ecdc5edf17f3eded66deb16f
Silthus <silthus@raid-craft.de>
2014-05-04 10:18:47 +0200

    fixed java doc errors

--- src/main/java/de/raidcraft/skills/api/trigger/Trigger.java
+++ src/main/java/de/raidcraft/skills/api/trigger/Trigger.java
@@ -6,22 +6,6 @@ package de.raidcraft.skills.api.trigger;
 import de.raidcraft.skills.api.character.CharacterTemplate;
 
 /**
- * Trigger superclass. should be extended as:
- * <pre>
- *     class MyTrigger extends Trigger<MyTrigger> {
- *         public static final HandlerList<MyTrigger> handlers = new HandlerList<MyTrigger>();
- *
- *         @Override
- *         HandlerList<MyTrigger> getHandlers() {
- *             return handlers;
- *         }
- *         @Override
- *         void call(Listener<MyTrigger> listener) {
- *             listener.onEvent(this);
- *         }
- *     }
- * </pre>
- *
  * @author lahwran
  */
 public abstract class Trigger {
