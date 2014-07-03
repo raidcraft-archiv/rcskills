4cbb05de10bab0d960a63c502668a268c6ae4dcc
Silthus <silthus@raid-craft.de>
2014-07-03 18:04:21 +0200

    added mob kill requirement

--- src/main/java/de/raidcraft/skills/api/character/AbstractCharacterTemplate.java
+++ src/main/java/de/raidcraft/skills/api/character/AbstractCharacterTemplate.java
@@ -81,6 +81,7 @@ public abstract class AbstractCharacterTemplate implements CharacterTemplate {
     private Attack lastAttack;
     private AttachedLevel<CharacterTemplate> attachedLevel;
     private boolean recalculateHealth = false;
+    private CharacterTemplate lastKill;
 
     public AbstractCharacterTemplate(LivingEntity entity) {
 
@@ -605,7 +606,7 @@ public abstract class AbstractCharacterTemplate implements CharacterTemplate {
             // also actually damage the entity
             int newHealth = (int) (getHealth() - attack.getDamage());
             if (newHealth <= 0) {
-                kill();
+                kill(attacker);
             } else {
                 setHealth(newHealth);
                 getEntity().playEffect(EntityEffect.HURT);
@@ -678,20 +679,33 @@ public abstract class AbstractCharacterTemplate implements CharacterTemplate {
         }
         RaidCraft.callEvent(new RCEntityDeathEvent(this));
         clearEffects();
-        getEntity().damage(getMaxHealth(), killer.getEntity());
+        getEntity().setCustomNameVisible(false);
+        // we need to damage not set health the entity or else it wont fire an death event
+        if (killer == null) {
+            getEntity().damage(getMaxHealth());
+        } else {
+            killer.setLastKill(this);
+            getEntity().damage(getMaxHealth(), killer.getEntity());
+        }
     }
 
     @Override
     public void kill() {
 
-        if (getEntity().isDead()) {
-            return;
-        }
-        getEntity().setCustomNameVisible(false);
-        RaidCraft.callEvent(new RCEntityDeathEvent(this));
-        clearEffects();
-        // we need to damage not set health the entity or else it wont fire an death event
-        getEntity().damage(getMaxHealth());
+        kill(null);
+    }
+
+    @Nullable
+    @Override
+    public CharacterTemplate getLastKill() {
+
+        return lastKill;
+    }
+
+    @Override
+    public void setLastKill(CharacterTemplate lastKill) {
+
+        this.lastKill = lastKill;
     }
 
     @Override
