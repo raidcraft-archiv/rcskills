91be2de07d186bc8b22fd11f968e8706bbba3c22
Denis Cundari <denis@cundari.de>
2014-07-06 16:54:23 +0200

    added a string representation of the tskill object

--- src/main/java/de/raidcraft/skills/tables/TSkill.java
+++ src/main/java/de/raidcraft/skills/tables/TSkill.java
@@ -6,9 +6,12 @@ import de.raidcraft.skills.SkillsPlugin;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Material;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
@@ -62,14 +65,56 @@ public class TSkill extends Model {
     private boolean hidden = true;
 
     /**
+     * The icon material the skill is using.
+     */
+    @Enumerated(EnumType.STRING)
+    private Material iconMaterial;
+
+    /**
+     * The character level requirement.
+     */
+    private int reqLevel = 1;
+
+    /**
      * When the skill mastery is reached.
      */
     private int maxLevel = 1;
 
     /**
+     * The length of the cooldown, or 0 if instant
+     */
+    private int cooldown = 0;
+
+    /**
      * The maximum distance of the skill to have an effect.
      */
     private int range = 1;
+
+    /**
+     * Only for debugging.
+     */
+    @Override
+    public String toString() {
+
+        final StringBuilder result = new StringBuilder();
+        final String newLine = System.getProperty("line.separator");
+
+        result.append(this.getClass().getName() + " Object {" + newLine);
+        result.append(" Key: " + this.getNameKey() + newLine);
+        result.append(" Profession: " + this.getProfession());
+        result.append(" Creation timestamp: " + this.getCretimestamp() + newLine);
+        result.append(" Update timestamp: " + this.getUpdtimestamp() + newLine);
+        result.append(" Enabled: " + this.isEnabled());
+        result.append(" Hidden: " + this.isHidden());
+        result.append(" Icon material: " + this.getIconMaterial());
+        result.append(" Required level: " + this.getReqLevel());
+        result.append(" Maximum level: " + this.getMaxLevel());
+        result.append(" Cooldown time: " + this.getCooldown());
+        result.append(" Range: " + this.getRange());
+        result.append("}");
+
+        return result.toString();
+    }
 
     public static Finder<String, TSkill> find = new TSkill.Finder<>(String.class, TSkill.class, SkillsPlugin.class);
 }
