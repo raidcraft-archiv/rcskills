e2ee733d935f4933f8bf75fe4330d5abbb838784
Silthus <silthus@raid-craft.de>
2014-07-25 17:12:38 +0200

    fixed major action api design flaw that did not use the config

--- src/main/java/de/raidcraft/skills/actions/AddHeroExpAction.java
+++ src/main/java/de/raidcraft/skills/actions/AddHeroExpAction.java
@@ -4,6 +4,7 @@ import de.raidcraft.RaidCraft;
 import de.raidcraft.api.action.action.Action;
 import de.raidcraft.skills.SkillsPlugin;
 import de.raidcraft.skills.api.hero.Hero;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 /**
@@ -12,10 +13,10 @@ import org.bukkit.entity.Player;
 public class AddHeroExpAction implements Action<Player> {
 
     @Override
-    public void accept(Player player) {
+    public void accept(Player player, ConfigurationSection config) {
 
         SkillsPlugin plugin = RaidCraft.getComponent(SkillsPlugin.class);
         Hero hero = plugin.getCharacterManager().getHero(player);
-        hero.getExpPool().addExp(getConfig().getInt("exp"));
+        hero.getExpPool().addExp(config.getInt("exp"));
     }
 }
