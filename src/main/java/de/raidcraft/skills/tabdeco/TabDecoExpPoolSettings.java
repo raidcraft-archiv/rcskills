d1f53067fce373e6683ea87616299212d9331750
Dragonfire <bastian616-git@yahoo.de>
2014-08-11 21:13:58 +0200

    try to fix null professiosn refs #835

--- src/main/java/de/raidcraft/skills/tabdeco/TabDecoExpPoolSettings.java
+++ src/main/java/de/raidcraft/skills/tabdeco/TabDecoExpPoolSettings.java
@@ -1,8 +1,6 @@
 package de.raidcraft.skills.tabdeco;
 
 import de.raidcraft.skills.SkillsPlugin;
-import de.raidcraft.skills.api.exceptions.UnknownProfessionException;
-import de.raidcraft.skills.api.exceptions.UnknownSkillException;
 import de.raidcraft.skills.api.hero.Hero;
 import de.raidcraft.skills.api.hero.Option;
 import de.raidcraft.tabdeco.api.TabDecoSetting;
@@ -27,10 +25,11 @@ public class TabDecoExpPoolSettings extends TabDecoSetting {
         if (settingName.equalsIgnoreCase("expPool")) {
             return String.valueOf(hero.getExpPool().getExp());
         } else if (settingName.equalsIgnoreCase("expPoolLink") && Option.EXP_POOL_LINK.get(hero) != null) {
-            try {
-                return hero.getProfession(Option.EXP_POOL_LINK.get(hero)).getFriendlyName();
-            } catch (UnknownSkillException | UnknownProfessionException ignored) {
-            }
+            // TODO: fix it
+            //            try {
+            //                return hero.getProfession(Option.EXP_POOL_LINK.get(hero)).getFriendlyName();
+            //            } catch (UnknownSkillException | UnknownProfessionException ignored) {
+            //            }
         }
         return "";
     }
