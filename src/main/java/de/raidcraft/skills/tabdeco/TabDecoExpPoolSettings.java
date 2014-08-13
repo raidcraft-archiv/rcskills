5540ae113ed6003c5ee30fa04c68b64bf6b8c2a8
Dragonfire <bastian616-git@yahoo.de>
2014-08-13 22:48:17 +0200

    hotfix refs #1381

--- src/main/java/de/raidcraft/skills/tabdeco/TabDecoExpPoolSettings.java
+++ src/main/java/de/raidcraft/skills/tabdeco/TabDecoExpPoolSettings.java
@@ -1,6 +1,8 @@
 package de.raidcraft.skills.tabdeco;
 
 import de.raidcraft.skills.SkillsPlugin;
+import de.raidcraft.skills.api.exceptions.UnknownProfessionException;
+import de.raidcraft.skills.api.exceptions.UnknownSkillException;
 import de.raidcraft.skills.api.hero.Hero;
 import de.raidcraft.skills.api.hero.Option;
 import de.raidcraft.tabdeco.api.TabDecoSetting;
@@ -24,12 +26,12 @@ public class TabDecoExpPoolSettings extends TabDecoSetting {
         Hero hero = plugin.getCharacterManager().getHero(player);
         if (settingName.equalsIgnoreCase("expPool")) {
             return String.valueOf(hero.getExpPool().getExp());
-        } else if (settingName.equalsIgnoreCase("expPoolLink") && Option.EXP_POOL_LINK.get(hero) != null) {
-            // TODO: fix it
-            //            try {
-            //                return hero.getProfession(Option.EXP_POOL_LINK.get(hero)).getFriendlyName();
-            //            } catch (UnknownSkillException | UnknownProfessionException ignored) {
-            //            }
+        } else if (settingName.equalsIgnoreCase("expPoolLink") && Option.EXP_POOL_LINK != null
+                && Option.EXP_POOL_LINK.get(hero) != null) {
+            try {
+                return hero.getProfession(Option.EXP_POOL_LINK.get(hero)).getFriendlyName();
+            } catch (UnknownSkillException | UnknownProfessionException ignored) {
+            }
         }
         return "";
     }
