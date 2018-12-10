cd55573392ad456bcbe923bcc08e3bbe9104cc76
Michael Reichenbach <michael@mirenoba.com>
2018-12-10 20:47:18 +0100

    feat(hotbar): block passive skills from drag and drop to hotbar

---
+++ src/main/java/de/raidcraft/skills/api/skill/Active.java
@@ -0,0 +1,7 @@
+package de.raidcraft.skills.api.skill;
+
+/**
+ * The skill is active and must be used directly by a player or entity to activate.
+ */
+public interface Active {
+}
--- src/main/java/de/raidcraft/skills/api/trigger/CommandTriggered.java
+++ src/main/java/de/raidcraft/skills/api/trigger/CommandTriggered.java
@@ -2,11 +2,12 @@ package de.raidcraft.skills.api.trigger;
 
 import com.sk89q.minecraft.util.commands.CommandContext;
 import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.skill.Active;
 
 /**
  * @author Silthus
  */
-public interface CommandTriggered {
+public interface CommandTriggered extends Active {
 
     void runCommand(CommandContext args) throws CombatException;
 }
